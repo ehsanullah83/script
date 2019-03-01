a75d399a31817ddcbb34465e9d61c2f4c5c7ecf0
ehsanullah83 <ehsanullah83@gmail.com>
2019-03-01 11:42:36 -0500

    few scripts to do common jobs

---
+++ pdf2table.sh
@@ -0,0 +1,4 @@
+## regular expressions for MVL reports
+(# MVL )([A-Za-z0-9-]+) #will match all MVL IDs
+(TEST: )([A-Z a-z0-9-/]+) # will match all the test names.
+(RESULT: )([A-Z a-z0-9-/]+) # will match all results.
