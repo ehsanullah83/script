e7f9af212f4a08048074a4d29d7a7766ef6742ba
ehsanullah83 <ehsanullah83@gmail.com>
2019-03-05 21:07:53 -0500

    deleted text processing related files

--- pdf2table.sh
+++
@@ -1,4 +0,0 @@
-## regular expressions for MVL reports
-(# MVL )([A-Za-z0-9-]+) #will match all MVL IDs
-(TEST: )([A-Z a-z0-9-/]+) # will match all the test names.
-(RESULT: )([A-Z a-z0-9-/]+) # will match all results.
