1fde0cf6521d3e2cecb70a6a08e0c7ce8788c1e6
ehsanullah83 <ehsanullah83@gmail.com>
2019-03-05 20:38:55 -0500

    working script

--- gene_inheritance.r
+++ gene_inheritance.r
@@ -1,42 +1,77 @@
+# This script will parse gene inheritance information from
+# omim genemap2.txt ( https://data.omim.org/downloads/WLpOojuyTTeoemLvEh7khg/genemap2.txt)
+# and will parse HGNC approved symbols downloaded from https://www.genenames.org/ 
+# to create an omim gene inheritance annotation file.  
 
-#import omim genemap2.txt
+#load the libraries.
 
-gm0 <- read_tsv(file="~/Desktop/genemap2.tsv")
+library(tidyverse)
 
-#Select data lines and required columns to omit metadata information.
-gm1 <- gm0[1:16866, ] %>% select(7,9,13)
+#import omim genemap2.txt from working directory.
 
-#Extract Inheritance patterns from column Phenotypes.
-gm3 <- gm1 %>% 
+genemap2 <- read.delim(file="genemap2.txt", header = TRUE, skip = 3)
+
+#Select data lines and required columns. And extract words describing inheritance.
+gm1 <- genemap2[1:16866, ] %>% 
+  select(1,2,3,7,13) %>% 
   mutate(Inheritance1 = str_extract(Phenotypes, "Autosomal recessive")) %>% 
   mutate(Inheritance2 = str_extract(Phenotypes, "Autosomal dominant")) %>% 
   mutate(Inheritance3 = str_extract(Phenotypes, "X-linked")) %>%
   mutate(Inheritance4 = str_extract(Phenotypes, "Digenic recessive"))
 
+# Remove NAs from columns 6 to 9.
+gm2 <- as.matrix(gm1) 
+indx <- which(is.na(gm2[, 6])==TRUE) 
+gm2[indx, 6] = "" 
+gm2  <-  data.frame(gm2) 
 
-# Remove NAs from columns 4 to 7.
-gm4 <- as.matrix(gm3) 
-indx <- which(is.na(gm4[, 4])==TRUE) 
-gm4[indx, 4] = "" 
-gm4 = data.frame(gm4) 
+gm2 <- as.matrix(gm2) 
+indx <- which(is.na(gm2[, 7])==TRUE) 
+gm2[indx, 7] = "" 
+gm2 = data.frame(gm2)
 
-gm4 <- as.matrix(gm4) 
-indx <- which(is.na(gm4[, 5])==TRUE) 
-gm4[indx, 5] = "" 
-gm4 = data.frame(gm4)
+gm2 <- as.matrix(gm2) 
+indx <- which(is.na(gm2[, 8])==TRUE) 
+gm2[indx, 8] = "" 
+gm2 = data.frame(gm2)
 
-gm4 <- as.matrix(gm4) 
-indx <- which(is.na(gm4[, 6])==TRUE) 
-gm4[indx, 6] = "" 
-gm4 = data.frame(gm4)
+gm2 <- as.matrix(gm2) 
+indx <- which(is.na(gm2[, 9])==TRUE) 
+gm2[indx, 9] = "" 
+gm2 = data.frame(gm2)
 
-gm4 <- as.matrix(gm4) 
-indx <- which(is.na(gm4[, 7])==TRUE) 
-gm4[indx, 7] = "" 
-gm4 = data.frame(gm4)
+#Unite columns 6,7,8,9
+gm3 <- unite(gm2, Inheritance, c(6,7,8,9), sep= "")
 
-#Unite
-gm5 <- unite(gm4, Inheritance, c(4,5,6,7), sep= "")
+#substitute spaces and add a separator 
 
-gm5$Inheritance <- gsub("([a-z])([A-Z])", "\\1 | \\2", gm5$Inheritance)
+gm3$Inheritance <- gsub("([a-z])([A-Z])", "\\1 | \\2", gm3$Inheritance)
+
+
+#unnest multiple gene symbols into rows. 
+gm4 <- gm3 %>% mutate(Gene.Symbols = strsplit(as.character(Gene.Symbols), ",")) %>% 
+  unnest(Gene.Symbols)
+
+# import HGNC gene table from working directory
+
+hgnc_genes <- read.delim(file='hgnc_genes.txt', header=TRUE)
+
+# select gene symbol and aliases. 
+hgnc_gs <- select(hgnc_genes, 2,9)
+
+#change column names to match with gm4$Gene.Symbols
+colnames(hgnc_gs) <- c("Gene.Symbols", "Aliases")
+
+# right join to get the match between hgnc approved symbols and gm4$Gene.Symbols
+gm5 <- gm4 %>% right_join(hgnc_gs, by = "Gene.Symbols")
+
+# Final table, sort, clean etc.
+omim_gene_inheritance <- gm5[order(gm5$Chromosome, gm5$Genomic.Position.Start), ] %>% 
+  filter(Inheritance != "") %>% 
+  select(1,2,3,6,5,4)
+
+#Save output
+
+write_tsv(omim_gene_inheritance, "omim_gene_inheritance.tsv")
+
 
